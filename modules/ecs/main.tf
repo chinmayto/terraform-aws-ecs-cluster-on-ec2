986adb5574a0ffab58fe15ea3634ee356d9cd8f6
chinmayto <chinmay13@gmail.com>
2024-03-23 23:24:05 +0530

    Commit2

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -124,7 +124,6 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
 ####################################################
 resource "aws_ecs_service" "ecs_service" {
   name            = "my-ecs-service"
-  launch_type     = "EC2"
   cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
   desired_count   = 4
